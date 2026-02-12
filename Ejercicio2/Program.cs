5aec4d7bad3afc7fdaa58e7742b8af7fc39355fc
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-12 15:34:17 -0400

    cargando

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número para verificar si es perfecto: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int suma = 0;
+            int i = 1;
+
+            while (i < numero)
+            {
+                if (numero % i == 0)
+                {
+                    suma += i;
+                }
+                i++;
+            }
+
+            if (suma == numero)
+            {
+                Console.WriteLine("\nEl número " + numero + " es un NÚMERO PERFECTO.");
+            }
+            else
+            {
+                Console.WriteLine("\nEl número " + numero + " NO es un número perfecto.");
+            }
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
