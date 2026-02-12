319a1e7a66efa1b61f4efa496681f0362104e1fc
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-12 16:27:44 -0400

    cargando

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Primer número: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Segundo número: ");
+        int b = int.Parse(Console.ReadLine());
+        int max = (a > b) ? a : b;
+        int mcm = max;
+        while (mcm % a != 0 || mcm % b != 0)
+        {
+            mcm += max;
+        }
+        Console.WriteLine("El MCM es: " + mcm);
+        Console.ReadKey();
+    }
+}
