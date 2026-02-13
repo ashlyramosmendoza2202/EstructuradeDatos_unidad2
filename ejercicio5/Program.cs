4712e046a42292865fb2a6c7454a35480a9e7f72
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 02:35:48 -0400

    cargando

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int mayor = int.MinValue;
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Ingrese número " + i + ": ");
+            int num = int.Parse(Console.ReadLine());
+            if (num > mayor) mayor = num;
+        }
+        Console.WriteLine("El número mayor es: " + mayor);
+        Console.ReadKey();
+    }
+}
