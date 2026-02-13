5ca254605d62ac554cc337c2f163f198ce17ce74
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:43:30 -0400

    cargando

---
+++ InversionLista/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> lista = new LinkedList<int>(new[] { 1, 2, 3, 4 });
+        Console.WriteLine("Antes: " + string.Join(" -> ", lista));
+
+        var invertida = new LinkedList<int>(lista.Reverse());
+        Console.WriteLine("Despues: " + string.Join(" -> ", invertida));
+    }
+}
