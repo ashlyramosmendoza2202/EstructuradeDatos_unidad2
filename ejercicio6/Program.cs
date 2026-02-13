d8652bc94a08801c21b8d6b33083b1108f03ce3c
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 02:41:10 -0400

    cargando

---
+++ ejercicio6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Cant: ");
+        int c = int.Parse(Console.ReadLine()), p = 0;
+        for (int i = 0; i < c; i++)
+        {
+            Console.Write("Num: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 2) continue;
+            bool esP = true;
+            for (int j = 2; j < n; j++) if (n % j == 0) esP = false;
+            if (esP) p++;
+        }
+        Console.WriteLine("Primos: " + p);
+        Console.ReadKey();
+    }
+}
