f83e580a128791c60afaad7b85715c7900a6e8a6
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:40:50 -0400

    cargando

---
+++ ListaenlazadaSimple/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> lista = new LinkedList<int>();
+
+        Console.Write("Numero para agregar: ");
+        lista.AddLast(int.Parse(Console.ReadLine()));
+
+        Console.Write("Numero para eliminar: ");
+        lista.Remove(int.Parse(Console.ReadLine()));
+
+        Console.Write("Numero para buscar: ");
+        bool existe = lista.Contains(int.Parse(Console.ReadLine()));
+
+        Console.WriteLine("Lista: " + string.Join(" -> ", lista));
+        Console.WriteLine("¿Existe?: " + existe);
+    }
+}
