7b2fd91ccd3e2fcba71484511f5384b679655ef5
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:13:04 -0400

    cargando

---
+++ ejercicio13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> lista = new List<int> { 15, 8, 25, 3, 10 };
+
+        Console.WriteLine("Máximo: " + lista.Max());
+        Console.WriteLine("Mínimo: " + lista.Min());
+    }
+}
