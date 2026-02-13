37a47af5041978588efbd95bc222e2618735298d
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:06:15 -0400

    cargando

---
+++ ejercicio12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> lista = new List<int> { 10, 20, 30, 40 };
+        int suma = lista.Sum();
+        Console.WriteLine("La suma es: " + suma);
+    }
+}
