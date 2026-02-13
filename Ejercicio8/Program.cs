26130cd93b7544d1a27e1e4d7caa78fa208ab6f3
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 02:45:45 -0400

    cargando

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int pos = 0;
+        int neg = 0;
+        int cero = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0) pos++;
+            if (n < 0) neg++;
+            if (n == 0) cero++;
+        }
+
+        Console.WriteLine(pos);
+        Console.WriteLine(neg);
+        Console.WriteLine(cero);
+    }
+}
