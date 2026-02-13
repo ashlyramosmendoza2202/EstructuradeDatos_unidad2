e666e5b7fd802b8b47eeab501148968eea357dcd
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:19:00 -0400

    cargando

---
+++ ejercicio15/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> lista = new List<int> { 10, 20, 30, 40 };
+        double promedio = lista.Average();
+        Console.WriteLine("El promedio es: " + promedio);
+    }
+}
