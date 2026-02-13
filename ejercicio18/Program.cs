9f5df33646a68af3f0eaa5b57352f7a4ea54f893
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:28:29 -0400

    cargando

---
+++ ejercicio18/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> lista = new List<string> { "Manzana", "Pera", "Uva" };
+
+        Console.Write("Elemento a buscar: ");
+        string buscar = Console.ReadLine();
+
+        int posicion = lista.IndexOf(buscar);
+
+        if (posicion != -1) Console.WriteLine("Posicion: " + posicion);
+        else Console.WriteLine("No se encontro");
+    }
+}
