b266fe5dc9c7ec221a473604bc0e46f064f40314
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:35:00 -0400

    cargando

---
+++ OperacionesconArreglos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> lista = new List<int> { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Lista inicial: " + string.Join(", ", lista));
+
+ 
+        Console.Write("\nNumero a insertar: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("En que posicion: ");
+        int p = int.Parse(Console.ReadLine());
+        lista.Insert(p, n);
+
+        Console.Write("\nPosicion para eliminar: ");
+        int e = int.Parse(Console.ReadLine());
+        lista.RemoveAt(e);
+
+
+        Console.Write("\nNumero a buscar: ");
+        int b = int.Parse(Console.ReadLine());
+        int ind = lista.IndexOf(b);
+
+        Console.WriteLine("\n--- RESULTADOS ---");
+        Console.WriteLine("Lista final: " + string.Join(", ", lista));
+        Console.WriteLine("El numero buscado esta en el indice: " + ind);
+    }
+}
