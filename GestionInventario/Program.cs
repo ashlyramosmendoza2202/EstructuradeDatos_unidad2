443b104b193882e3a13fe9a1fb54762532d182de
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:46:52 -0400

    cargando

---
+++ GestionInventario/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+    
+        List<string> inv = new List<string> { "Laptop", "Mouse", "Teclado" };
+
+        inv.Add("Monitor"); 
+        inv.Remove("Mouse"); 
+
+        Console.Write("Buscar producto: ");
+        string p = Console.ReadLine();
+        Console.WriteLine(inv.Contains(p) ? "Disponible" : "No existe");
+        Console.WriteLine("Inventario: " + string.Join(", ", inv));
+    }
+}
