c86dae7ed70cc203a699e37205e89d3b64fd5aa9
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 02:56:10 -0400

    cargando

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string s = "";
+        while (s != "s")
+        {
+            Console.Write("N1: "); double a = double.Parse(Console.ReadLine());
+            Console.Write("N2: "); double b = double.Parse(Console.ReadLine());
+            Console.Write("Op: "); string o = Console.ReadLine();
+            if (o == "+") Console.WriteLine(a + b);
+            if (o == "-") Console.WriteLine(a - b);
+            if (o == "*") Console.WriteLine(a * b);
+            if (o == "/") Console.WriteLine(a / b);
+            Console.Write("¿Salir? (s/n): "); s = Console.ReadLine();
+        }
+    }
+}
