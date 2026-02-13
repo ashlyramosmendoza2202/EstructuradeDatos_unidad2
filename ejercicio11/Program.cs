fa01e1edbab885c388ee6f286aaedfbd703c1f44
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:02:20 -0400

    cargando

---
+++ ejercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Oracion: ");
+        string entrada = Console.ReadLine().ToLower();
+        string p = new string(entrada.Where(c => !char.IsWhiteSpace(c)).ToArray());
+        string r = new string(p.Reverse().ToArray());
+
+        if (p == r) Console.WriteLine("Es palindroma");
+        else Console.WriteLine("No es palindroma");
+    }
+}
