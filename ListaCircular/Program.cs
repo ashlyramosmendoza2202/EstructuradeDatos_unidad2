673824dc0738d4a5880ef7342fdae92cf3c0628f
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:45:18 -0400

    cargando

---
+++ ListaCircular/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> circular = new List<int> { 10, 20, 30 };
+        // Simulación de recorrido circular (3 vueltas)
+        for (int i = 0; i < 9; i++)
+        {
+            Console.Write(circular[i % circular.Count] + " ");
+        }
+    }
+}
