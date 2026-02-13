c2b4d474d86962ff09fab4b030cc957a61f9b258
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 03:20:12 -0400

    cargando

---
+++ ejercicio16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> lista = new List<int> { 1, 2, 3, 4, 5 };
+
+        lista.Reverse();
+
+        foreach (int x in lista) Console.Write(x + " ");
+    }
+}
