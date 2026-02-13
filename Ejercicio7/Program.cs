f56058bbf11f9bc10f89b3873315d1a7458ccf45
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-13 02:42:35 -0400

    cargando

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Inicio: "); int a = int.Parse(Console.ReadLine());
+        Console.Write("Fin: "); int b = int.Parse(Console.ReadLine());
+        for (int i = a; i <= b; i++) if (i % 5 == 0) Console.Write(i + " ");
+        Console.ReadKey();
+    }
+}
